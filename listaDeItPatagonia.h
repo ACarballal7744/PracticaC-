b95e603f59e1cb055438b9748231e9787046e458
ACarballal7744 <alan.carba@hotmail.com>
2021-11-29 13:54:53 -0300

    lista iterando

--- listaDeItPatagonia.h
+++ listaDeItPatagonia.h
@@ -1,13 +1,46 @@
 #pragma once
 #include <iostream>
 #include <string>
-
+#include <list>
 using namespace std;
+
+struct persona {
+	string nombre;
+	string apellido;
+};
 
 class listaDeItPatagonia
 {
 	private:
-		string list;
+		list <persona> personas;
+		
+	public:
+		void agregarPersona(string nombre, string apellido) {
+			persona ingresante;
+			ingresante.nombre = nombre;
+			ingresante.apellido = apellido;
+			personas.push_back(ingresante);
+		}
+
+		void sacarPersona() {
+			personas.pop_back();
+		}
+
+		list <persona> getListaPersonas() {
+			return personas;
+		}
+		
+		persona buscarPrimeraNombre() {
+						
+			for (persona nombre:personas)
+			{
+				return nombre;
+			}
+
+		}
+
+		
+
 		
 };
 
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+#include "listaDeItPatagonia.h"
+
+using namespace std;
+
+int main() {
+
+	listaDeItPatagonia personas;
+
+	personas.agregarPersona("Alan", "Carballal");
+
+	cout << personas.buscarPrimeraNombre().nombre << endl;
+
+	
+
+
+
+
+
+}
